059864624776516f74d2e915f45897f5a5ef3828
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-09 23:13:58 +0200

    add persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { wrapper } from "@/redux/store";
 import { Layout } from "@/components/Layout/Layout";
+import { ReactReduxContext } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ReactReduxContext.Consumer>
+      {({ store }) => (
+        // @ts-ignore
+        <PersistGate persistor={store.__persistor}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PersistGate>
+      )}
+    </ReactReduxContext.Consumer>
   );
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,52 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { FilmsReducer } from "@/redux/slices/films";
 import { createWrapper } from "next-redux-wrapper";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 export function makeStore() {
-  return configureStore({
-    reducer: {
-      films: FilmsReducer,
-    },
-  });
+  const isServer = typeof window === "undefined";
+  if (isServer) {
+    return configureStore({
+      reducer: {
+        films: FilmsReducer,
+      },
+    });
+  } else {
+    const persistConfig = {
+      key: "nextjs",
+      whitelist: ["savedFilms"], // make sure it does not clash with server keys
+      storage,
+    };
+
+    const persistedReducer = persistReducer(persistConfig, FilmsReducer);
+
+    const store = configureStore({
+      reducer: {
+        films: persistedReducer,
+      },
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          immutableCheck: false,
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+    });
+
+    //@ts-ignore
+    store.__persistor = persistStore(store);
+    return store;
+  }
 }
 
 export const store = makeStore();
